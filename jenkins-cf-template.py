c9e3667167e3a7d0bb4f585094653de2bdd2874e
Stan Zhou <stan.osaka@gmail.com>
2019-08-15 15:20:30 +1000

    Revert "Allowing Jenkins to interact with CodePipeline"
    This reverts commit 54bd4e39efbd94819723e82f8dfc25b419a14def.

--- jenkins-cf-template.py
+++ jenkins-cf-template.py
@@ -100,19 +100,6 @@ t.add_resource(InstanceProfile(
     Roles=[Ref("Role")]
 ))
 
-t.add_resource(IAMPolicy(
-    "Policy",    
-    PolicyName="AllowS3",    
-    PolicyDocument=Policy(        
-        Statement=[            
-          Statement(                
-             Effect=Allow,                
-             Action=[Action("s3", "*")],                
-             Resource=["*"])        
-        ]    
-     ),        
-))
-
 t.add_resource(ec2.Instance(
     "instance",
     ImageId="ami-33f92051",
